aa8d277eb80d91a60b40dd8b27155ec0ac218bb9
RayRutjes <r.rutjes@devarium.fr>
2016-01-30 22:36:21 +0100

    Added AskWhile feature.

--- ask.go
+++ ask.go
@@ -2,6 +2,7 @@ package ask
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 )
@@ -11,7 +12,7 @@ var (
 	in  io.Reader = os.Stdin
 )
 
-// Ask writes a question into the standard output and reads the answer from the
+// Ask writes a question to the standard output and reads the answer from the
 // standard input.
 func Ask(q string) (string, error) {
 	return Fask(q, out, in)
@@ -21,12 +22,39 @@ func Ask(q string) (string, error) {
 func Fask(q string, w io.Writer, r io.Reader) (string, error) {
 	q += ": "
 	if _, err := w.Write([]byte(q)); err != nil {
-		return "", err
+		return "", fmt.Errorf("Unable to write the question %v", err)
 	}
 	a, err := bufio.NewReader(r).ReadString('\n')
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("Unable to read the answer %v", err)
 	}
 
 	return a[:len(a)-1], err
+}
+
+// ValidateFunc is a function that is used to validate that an answer matches
+// the expectations. It should return a boolean and an additional validation failure message
+// when the validation fails.
+type ValidateFunc func(string) (bool, string)
+
+// AskWhile writes a question and returns the answer once the validation function
+// returns true. The questions is written again on each failed validation pass.
+func AskWhile(q string, v ValidateFunc) (string, error) {
+	return FaskWhile(q, v, out, in)
+}
+
+// FaskWhile writes a question and returns the answer once the validation function
+// returns true. The questions is written again on each failed validation pass.
+func FaskWhile(q string, v ValidateFunc, w io.Writer, r io.Reader) (string, error) {
+	for {
+		a, err := Fask(q, w, r)
+		if err != nil {
+			return "", err
+		}
+		ok, msg := v(a)
+		if ok {
+			return a, nil
+		}
+		w.Write([]byte(msg + "\n"))
+	}
 }
